13c9273cc464359d7de0d4ca4a3b28a8704fd63a
Marcos Roriz <marcosrorizinf@gmail.com>
2025-11-04 18:55:46 -0300

    Down LLM docker

--- mix_down_llm/down_llm.sh
+++ mix_down_llm/down_llm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/home/grupo_fctufg/anaconda3/envs/ra/bin/python -u /home/grupo_fctufg/down_llm.py >> /home/grupo_fctufg/logs/$(date +\%Y-\%m-\%d)-llm.txt
+python -u down_llm.py >> /home/grupo_fctufg/logs/$(date +\%Y-\%m-\%d)-llm.txt
 

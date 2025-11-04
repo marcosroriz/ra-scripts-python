725a258105d58b6153277930e0351509062b4914
Marcos Roriz <marcosrorizinf@gmail.com>
2025-11-04 19:13:52 -0300

    Docker down pecas

--- mix_down_pecas/down_pecas.sh
+++ mix_down_pecas/down_pecas.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/home/grupo_fctufg/anaconda3/envs/ra/bin/python -u /home/grupo_fctufg/down_pecas.py >> /home/grupo_fctufg/logs/$(date +\%Y-\%m-\%d)-pecas.txt
+python -u down_pecas.py >> /home/grupo_fctufg/logs/$(date +\%Y-\%m-\%d)-pecas.txt
 

299b579ce80d0abf6eb08adf40aeadbc8e67bd73
Marcos Roriz <marcosrorizinf@gmail.com>
2025-11-04 21:10:40 -0300

    Monitoramento de OS docker

--- monitoramento_regra_os/monitoramento_job.sh
+++ monitoramento_regra_os/monitoramento_job.sh
@@ -4,21 +4,8 @@
 echo "INICIO DO SCRIPT"
 echo $(date)
 
-# CD na pasta do script
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-cd "$SCRIPT_DIR"
-
-echo "Diretório atual"
-echo $SCRIPT_DIR
-
-# Sourcing do conda.sh para permitir 'conda activate'
-source ~/anaconda3/etc/profile.d/conda.sh
-
-# Ativa o ambiente 'ra'
-conda activate ra
-
 # Executa o script
-python monitoramento_regra_os.py >> /home/grupo_fctufg/logs/$(date +\%Y-\%m-\%d)-monitoramento-regra-os.txt
+python -u monitoramento_regra_os.py >> /home/grupo_fctufg/logs/$(date +\%Y-\%m-\%d)-monitoramento-regra-os.txt
 
 echo "TERMINO DO SCRIPT"
 echo $(date)
---
+++ monitoramento_regra_os/start_cron_monitoramento_regra_os.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+echo "=Iniciando container MIX MONITORAMENTO OS..."
+echo "=Iniciando container na data $(date '+%Y-%m-%d %H:%M:%S') ===="
+echo "=Horário do cron: ${CONTAINER_CRON_MONITORAMENTO_REGRA_OS}"
+
+# 1️ Executa o script no início se CONTAINER_EXEC_NO_INICIO_MONITORAMENTO_REGRA_OS for True
+if [ "${CONTAINER_EXEC_NO_INICIO_MONITORAMENTO_REGRA_OS}" = "True" ] || [ "${CONTAINER_EXEC_NO_INICIO_MONITORAMENTO_REGRA_OS}" = "true" ]; then
+    echo "CONTAINER_EXEC_NO_INICIO_MONITORAMENTO_REGRA_OS está ativo. Executando script imediatamente..."
+    bash /app/monitoramento_job.sh
+else
+    echo "CONTAINER_EXEC_NO_INICIO_MONITORAMENTO_REGRA_OS não está ativo. O script será executado apenas pelo cron."
+fi
+
+# 2️ Adiciona a linha do cron dinamicamente
+echo "${CONTAINER_CRON_MONITORAMENTO_REGRA_OS} /app/monitoramento_job.sh" | crontab -
+
+# 3️ Mostra o crontab atual (útil para debug)
+echo "Crontab atual:"
+crontab -l
+echo 
+
+# 4️ Inicia o cron em foreground (necessário para manter o container vivo)
+exec cron -f
